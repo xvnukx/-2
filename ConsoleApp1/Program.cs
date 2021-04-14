f8c5434ccb7ffd80fd9491764b089f21460a7f28
Владислав Дыляев <xsinkinxsinkinx@gmail.com>
2021-04-14 22:07:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите URL директории: ");
+
+                string path = Console.ReadLine();
+                var kek = GetF(path);
+                Getss(path);
+                Console.WriteLine($"Размер папки {kek} байт");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        static void Getss(string path)
+        {
+
+            if (Directory.Exists(path))
+            {
+                string[] dir = Directory.GetDirectories(path);
+                foreach (var item in dir)
+                {
+                    if (Directory.Exists(item))
+                    {
+                        Getss(item);
+                        GetF(item);
+                    }
+
+                }
+
+
+            }
+
+        }
+        static long GetF(string path)
+        {
+            long count = 0;
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo f in files)
+            {
+
+                {
+                    Console.WriteLine($"{f}, {f.Length}");
+                    count += f.Length;
+                }
+
+            }
+
+            return count;
+
+        }
+    }
+}
